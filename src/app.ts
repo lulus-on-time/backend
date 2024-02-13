0245c38a06b152421672c39c1fec8260f5d67300
Samuel Panggabean <sampanggabean22@gmail.com>
2024-02-13 11:33:21 +0700

    Remove express as it is not needed for websocket

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,9 @@
-import express, { Express, Request, Response } from 'express';
 import { WebSocketServer } from 'ws';
 import connectionEventListener from './positioning/wss-listener/connection';
 import dotenv from 'dotenv';
 
-const app: Express = express();
-
 dotenv.config();
-const host = process.env.HOST;
-const port = Number(process.env.SERVERPORT);
-const wsPort = Number(process.env.WSSERVERPORT);
-
-app.get('/', async (req: Request, res: Response) => {
-  res.status(404).send({
-    code: 404,
-    errors: {
-      reason:
-        `This server only accepts websocket connections at port ${wsPort}`,
-    },
-    data: null,
-  });
-});
-
-app.listen(port, host != undefined ? host : '0.0.0.0', () => {
-  console.log(`Find Myself server listening on port: ${port}`);
-});
+const wsPort = Number(process.env.PORT);
 
 const wss = new WebSocketServer({
   port: wsPort,
