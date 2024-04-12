a4ea23b79dca9f38c1a928e1c9fcde00e8b8918c
Samuel Panggabean <sampanggabean22@gmail.com>
2024-04-12 18:39:33 +0700

    Use Cors in API

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import roomRouter from './floor/router';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.listen(port, () => {
   console.log('Express app running on port ' + port);
