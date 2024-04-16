b2ab82cdfeeb9c83bf15bc8807d878945f160f9d
Samuel Panggabean <sampanggabean22@gmail.com>
2024-04-16 14:37:10 +0700

    Read All AP

--- src/api/aps/router.ts
+++ src/api/aps/router.ts
@@ -3,6 +3,17 @@ import validation from './validation';
 import { AccessPoint } from '@prisma/client';
 import prisma from '../../db/prisma-client';
 
+type ApResponse = {
+  key: number;
+  floor: {
+    id: number;
+    name: string;
+    level: number;
+    apTotal: number;
+  };
+  locationName: string;
+};
+
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
@@ -55,6 +66,51 @@ router.post('/create', async (req, res) => {
     }
 
     res.status(200).send(response);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const floors = await prisma.floor.findMany({
+      orderBy: {
+        level: 'asc',
+      },
+      include: {
+        rooms: {
+          include: {
+            accessPoints: true,
+          },
+          orderBy: {
+            id: 'asc',
+          },
+        },
+      },
+    });
+
+    const apResponse: ApResponse[] = [];
+
+    for (const floor of floors) {
+      for (const room of floor.rooms) {
+        for (const ap of room.accessPoints) {
+          apResponse.push({
+            key: ap.id,
+            floor: {
+              id: floor.id,
+              name: floor.name,
+              level: floor.level,
+              apTotal: room.accessPoints.length,
+            },
+            locationName: room.name,
+          });
+        }
+      }
+    }
+
+    res.send(apResponse);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('An unknown error occurred');
+    return;
   }
 });
 
