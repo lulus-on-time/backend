8691fb1e37cc38c3e95f1638ff09b3c97111a5ae
Samuel Panggabean <sampanggabean22@gmail.com>
2024-05-22 10:55:38 +0700

    Make machine learning url to environment variable

--- src/wss/positioning/socketio-client/client.ts
+++ src/wss/positioning/socketio-client/client.ts
@@ -1,5 +1,15 @@
+import { configDotenv } from 'dotenv';
 import { io } from 'socket.io-client';
 
-const client = io('http://34.101.70.143:5000', { forceNew: true });
+configDotenv();
+
+const machineLearningServerUrl = process.env.MLURL;
+
+const client = io(
+  machineLearningServerUrl !== undefined
+    ? machineLearningServerUrl
+    : 'http://34.101.70.143:5000',
+  { forceNew: true },
+);
 
 export default client;
